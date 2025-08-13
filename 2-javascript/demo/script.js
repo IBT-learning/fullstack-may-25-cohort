b7ef749448cc89aab59f6da1da7dbe276a383dbf
More THAN A CONQUEROR <danielekpegey@gmail.com>
2025-08-13 13:04:58 +0100

    grid assignment

--- 2-javascript/demo/script.js
+++ 2-javascript/demo/script.js
@@ -7,3 +7,8 @@ console.log(address);
 let num1 = 45;
 let num2 = 15;
 console.log(num1 + num2);
+let myArray =["Peter", "John", "Sarah"]
+let secondArray = new Array()
+let item = myArray[0];
+myArray[3] = "John"
+console.log(second);
